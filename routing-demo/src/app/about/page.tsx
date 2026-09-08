e2b3e0b7ee4ac09f09ac14b8484b870907c67a31
ratnadeep2003 <ratnadeepabitkar@gmail.com>
2026-09-08 11:45:06 +0530

    understood file based routing

---
+++ routing-demo/src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function  About(){
+    return(
+        <div>
+            <h1> About me page </h1>
+        </div>
+    )
+}
---
+++ routing-demo/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "GitArch",
+  description: "Your architecture project",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
